be4f6ad504b716e601e5deb275b14a2d883d66db
Alexander Swerdlow <aswerdlow1@gmail.com>
2023-09-17 12:59:47 -0700

    Update library_ops.py

--- src/image_utils/library_ops.py
+++ src/image_utils/library_ops.py
@@ -1,5 +1,7 @@
-import lovely_tensors as lt
-from lovely_numpy import lovely, set_config
-
-lt.monkey_patch()
-set_config(repr=lovely)
+try:
+    import lovely_tensors as lt
+    from lovely_numpy import lovely, set_config
+    lt.monkey_patch()
+    set_config(repr=lovely)
+except ImportError:
+    print("lovely_tensors is not installed. Run `pip install lovely-tensors` if you wish to use it.")
