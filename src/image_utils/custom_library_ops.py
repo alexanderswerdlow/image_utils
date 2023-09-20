5e1d5db5f92f0f31b707a057ada70844b67045a9
Alexander Swerdlow <aswerdlow1@gmail.com>
2023-09-20 18:35:41 -0400

    Fix custom_library_ops

--- src/image_utils/custom_library_ops.py
+++ src/image_utils/custom_library_ops.py
@@ -16,7 +16,7 @@ def generic_print(self, arr_values):
     else:
         lib = torch
         num_elements = lib.prod(torch.tensor(list(self.shape))).item()
-        device = self.device.type
+        device = f'{self.device.type} '
 
     if self.dtype in (np.bool_, torch.bool):
         specific_data = f' sum: {self.sum()}, unique: {len(lib.unique(self))},'
@@ -27,7 +27,7 @@ def generic_print(self, arr_values):
 
     shape_str = ",".join([str(self.shape[i]) for i in range(len(self.shape))])
     finite_str = "finite" if lib.isfinite(self).all() else "non-finite"
-    basic_info = f'[{shape_str}] {self.dtype} {device} {finite_str}'
+    basic_info = f'[{shape_str}] {self.dtype} {device}{finite_str}'
     numerical_info = f'\nelems: {num_elements},{specific_data} min: {self.min():.3f}, max: {self.max().item():.3f}'
 
     def get_first_and_last_lines(text):
@@ -41,12 +41,15 @@ def generic_print(self, arr_values):
 
     return basic_info + numerical_info + f'\n{arr_values}\n' + basic_info
 
-# torch.set_printoptions(sci_mode=False, precision=3, threshold=10, edgeitems=2, linewidth=120)
-# normal_repr = torch.Tensor.__repr__
-# torch.Tensor.__repr__ = lambda self: generic_print(self, normal_repr(self))
+torch.set_printoptions(sci_mode=False, precision=3, threshold=10, edgeitems=2, linewidth=120)
+normal_repr = torch.Tensor.__repr__
+torch.Tensor.__repr__ = lambda self: generic_print(self, normal_repr(self))
 
-# np.set_printoptions(suppress=True, precision=3, threshold=10, edgeitems=2, linewidth=120)
-# np.set_string_function(lambda self: generic_print(self, np.ndarray.__repr__(self)), repr=False)
+np.set_printoptions(suppress=True, precision=3, threshold=10, edgeitems=2, linewidth=120)
+import copy
+
+normal_repr_ = np.ndarray.__str__
+np.set_string_function(lambda self: generic_print(self, normal_repr_(self)), repr=True)
 
 def set_random_seeds():
     torch.manual_seed(0)
--- src/image_utils/library_ops.py
+++ src/image_utils/library_ops.py
@@ -1,4 +1,8 @@
 try:
+    import torch
+    import numpy as np
+    torch.set_printoptions(sci_mode=False, precision=3, threshold=10, edgeitems=2, linewidth=120)
+    np.set_printoptions(suppress=True, precision=3, threshold=10, edgeitems=2, linewidth=120)
     import lovely_tensors as lt
     from lovely_numpy import lovely, set_config
     lt.monkey_patch()
